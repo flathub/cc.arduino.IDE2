70ad2a838ce6eba1bfafb4737119f152c3f40277
Joshua Glass <28513051+joshuag1000@users.noreply.github.com>
2022-09-30 21:07:08 +0100

    change ELECTRON_FLAGS to CUSTOM_IDE_FLAGS

--- arduino-ide.sh
+++ arduino-ide.sh
@@ -2,9 +2,9 @@
 
 if [[ $XDG_SESSION_TYPE == "wayland" ]] && [ -c /dev/nvidia0 ]
 then
-    ELECTRON_FLAGS="$ELECTRON_FLAGS --disable-gpu-sandbox"
+    CUSTOM_IDE_FLAGS="$CUSTOM_IDE_FLAGS --disable-gpu-sandbox"
 fi
 
 # I cracked zypak's secrets
 export ZYPAK_SPAWN_LATEST_ON_REEXEC=0
-env TMPDIR=$XDG_CACHE_HOME zypak-wrapper /app/arduino-ide/arduino-ide /app/arduino-ide/resources/app/scripts/arduino-ide-electron-main.js $ELECTRON_FLAGS "$@"
+env TMPDIR=$XDG_CACHE_HOME zypak-wrapper /app/arduino-ide/arduino-ide /app/arduino-ide/resources/app/scripts/arduino-ide-electron-main.js $CUSTOM_IDE_FLAGS "$@"
